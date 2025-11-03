2811f0c2ef7231b6ca83b9bd83c9a59573be2891
Alperen <alprncnky@gmail.com>
2025-11-03 20:38:02 +0300

    update

--- src/modules/feed/controllers/feed.controller.ts
+++ src/modules/feed/controllers/feed.controller.ts
@@ -1,11 +1,6 @@
 import { Query, Body, ParseIntPipe } from '@nestjs/common';
 import { CrudController } from '../../../common/decorators/crud-controller.decorator';
-import {
-  SaveEndpoint,
-  GetEndpoint,
-  GetListEndpoint,
-  DeleteEndpoint,
-} from '../../../common/decorators/endpoint.decorator';
+import { SaveEndpoint, GetEndpoint, GetListEndpoint, DeleteEndpoint } from '../../../common/decorators/endpoint.decorator';
 import { BaseController } from '../../../common/base/base-controller';
 import { Feed } from '../data/entities/feed.entity';
 import { SaveFeedDto } from '../contracts/requests/save-feed.dto';
@@ -15,30 +10,13 @@ import { FeedService } from '../business/services/feed.service';
 import { FeedRepository } from '../data/repositories/feed.repository';
 import { CriteriaDto } from '../../../common/dto/criteria.dto';
 
-/**
- * Feed Controller
- * Handles HTTP requests for Feed CRUD operations
- */
 @CrudController('feeds', 'Feed')
-export class FeedController extends BaseController<
-  Feed,
-  SaveFeedDto,
-  SaveFeedDto,
-  FeedResponseDto,
-  FeedListResponseDto
-> {
+export class FeedController extends BaseController<Feed, SaveFeedDto, SaveFeedDto, FeedResponseDto, FeedListResponseDto> {
   constructor(
     private readonly feedService: FeedService,
     private readonly feedRepository: FeedRepository,
   ) {
-    super(
-      feedService,
-      feedRepository,
-      FeedResponseDto,
-      FeedListResponseDto,
-      'Feed',
-      SaveFeedDto,
-    );
+    super(feedService, feedRepository, FeedResponseDto, FeedListResponseDto, 'Feed', SaveFeedDto);
   }
 
   @SaveEndpoint(SaveFeedDto, FeedResponseDto)
